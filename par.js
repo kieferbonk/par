f18c24a54864fa38fd0d18fc503c85fb72310b92
kieferbonk <sartanen@gmail.com>
2015-05-16 00:10:22 +0300

    basic CR(u)D for users + courses

--- par.js
+++ par.js
@@ -1,17 +1,47 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+Users = new Mongo.Collection('users');
+Courses = new Mongo.Collection('courses');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+if (Meteor.isClient) {
+
+  Template.list__courses.helpers({
+    courses: function () {
+      return Courses.find({});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.reg__user.events({
+    'submit .user--add': function (event) {
+      var name = event.target.user.value;
+      
+      Users.insert({
+        username: name,
+        createdAt: new Date()
+      });
+
+      event.target.user.value = '';
+
+      return false; 
+    }
+  });
+
+  Template.reg__course.events({
+    'submit .course--add': function (event) {
+      var title = event.target.course.value;
+      
+      Courses.insert({
+        title: title,
+        createdAt: new Date()
+      });
+
+      event.target.course.value = '';
+
+      return false; 
+    }
+  });
+
+  Template.list__courses.events({
+    'click .course--del': function () {
+      Courses.remove(this._id);
     }
   });
 }
